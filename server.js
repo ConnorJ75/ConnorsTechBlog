2e830a4ae7efa9b03ef9a38329ac17f3473c84d6
Connor Johnson <connorjohnson@connors-air.mynetworksettings.com>
2024-04-10 22:14:04 -0400

    reset server.js

--- server.js
+++ server.js
@@ -1,60 +1,44 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers: require("./utils/helpers") });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ 
-  helpers: {
-      formatPhoneNumber: function(phone){
-        var cleaned =  ('' + phone).replace(/\D/g, '');
-        var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
-        if (match) {
-          var intlCode = (match[1] ? '+1 ' : ' ');
-          console.log("Here");
-          console.log(match[2] + " / " + match[3] + " / " + match[3]);
-          return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
-        }
-        return null;
-      }
-  }
-});
-
 const sess = {
-  secret: 'Secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
 };
 
 app.use(session(sess));
-
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static("public"));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(
+    session({
+      secret: process.env.SECRET,
+      store: new SequelizeStore({ db: sequelize }),
+      resave: false,
+      saveUninitialized: false,
+    })
+);
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+    app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+  });
